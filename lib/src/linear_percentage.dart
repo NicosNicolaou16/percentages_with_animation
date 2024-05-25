61a72797984028b3b38eb9c2d71aa1982499d7bd
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-05-25 15:22:34 +0300

    customazation

--- lib/src/linear_percentage.dart
+++ lib/src/linear_percentage.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LinearPercentage extends StatelessWidget {
+class LinearPercentage extends StatefulWidget {
   final double currentPercentage;
   final double maxPercentage;
   final double height;
   final double heightPercentage;
-  int? duration;
-  Decoration? decoration;
-  Decoration? percentageDecoration;
+  final int? duration;
+  final Decoration? decoration;
+  final Decoration? percentageDecoration;
+  final Color backgroundPercentage;
+  final Color percentage;
 
   LinearPercentage({
     super.key,
@@ -18,32 +20,45 @@ class LinearPercentage extends StatelessWidget {
     this.duration,
     this.decoration,
     this.percentageDecoration,
-  }) : assert(currentPercentage <= maxPercentage);
+    this.backgroundPercentage = Colors.black26,
+    this.percentage = Colors.black,
+  })  : assert(currentPercentage <= maxPercentage),
+        assert(currentPercentage >= 0);
+
+  @override
+  State<LinearPercentage> createState() => _LinearPercentageState();
+}
+
+class _LinearPercentageState extends State<LinearPercentage> {
+  final int _delay = 1000;
+
+  double get _maxWidget => MediaQuery.of(context).size.width;
+
+  double get _percentage =>
+      (widget.currentPercentage / widget.maxPercentage) * _maxWidget;
 
   @override
   Widget build(BuildContext context) {
-    double x = MediaQuery.of(context).size.width;
-    double percent = (40 / 100) * x;
     return Stack(
       children: [
         Container(
-          width: x,
-          height: height,
-          color: Colors.black26,
-          decoration: decoration,
+          width: _maxWidget,
+          height: widget.height,
+          color: widget.backgroundPercentage,
+          decoration: widget.decoration,
         ),
         TweenAnimationBuilder(
-          duration: Duration(milliseconds: duration ?? 1000),
+          duration: Duration(milliseconds: widget.duration ?? _delay),
           curve: Curves.easeInOut,
           tween: Tween<double>(
             begin: 0,
-            end: percent,
+            end: _percentage,
           ),
           builder: (context, value, _) => Container(
             width: value,
-            height: heightPercentage,
-            color: Colors.black,
-            decoration: percentageDecoration,
+            height: widget.heightPercentage,
+            color: widget.percentage,
+            decoration: widget.percentageDecoration,
           ),
         ),
       ],
