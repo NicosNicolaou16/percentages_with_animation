b56650fd32cba3cf9e776af25b5e282dd42320fb
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-05-26 18:58:11 +0300

    minor change

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,11 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Percentage With Animation Example',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
