a0c0c293afac50393ed85997d6f9fabfcabf3c8e
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-03-31 00:52:17 +0300

    add the main class for wave perecentage

---
+++ lib/src/custom_painter/wave_painter.dart
@@ -0,0 +1,97 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class WavePainter extends CustomPainter {
+  final double fillPercentage;
+  final double waveFrequency;
+  final double waveAmplitude;
+  final double wavePhase;
+  final Color waveColor;
+
+  WavePainter({
+    this.fillPercentage = 0.0,
+    this.waveFrequency = 2.0,
+    this.waveAmplitude = 1.0,
+    this.wavePhase = 0.0,
+    this.waveColor = Colors.blue,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = waveColor
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 2.0;
+
+    final wavePath = Path();
+    final fillHeight = size.height * (1 - fillPercentage);
+    final centerX = size.width / 2;
+
+    // Starting point with phase
+    wavePath.moveTo(
+        0,
+        fillHeight +
+            waveAmplitude *
+                sin(waveFrequency * (-centerX) / size.width * 2 * pi +
+                    pi / 2 +
+                    wavePhase));
+    double previousY = fillHeight +
+        waveAmplitude *
+            sin(waveFrequency * (-centerX) / size.width * 2 * pi +
+                pi / 2 +
+                wavePhase);
+
+    for (double x = 0; x <= size.width; x++) {
+      // Incorporate phase into the wave calculation
+      final y = fillHeight +
+          waveAmplitude *
+              sin(waveFrequency * (x - centerX) / size.width * 2 * pi +
+                  pi / 2 +
+                  wavePhase);
+
+      if (x == 0) {
+        wavePath.moveTo(x, y);
+      } else {
+        wavePath.quadraticBezierTo(
+          x - 0.5,
+          (y + previousY) / 2,
+          x,
+          y,
+        );
+      }
+      previousY = y;
+    }
+
+    // Ending point with phase
+    wavePath.lineTo(
+        size.width,
+        fillHeight +
+            waveAmplitude *
+                sin(waveFrequency *
+                        (size.width - centerX) /
+                        size.width *
+                        2 *
+                        pi +
+                    pi / 2 +
+                    wavePhase));
+
+    wavePath.lineTo(size.width, size.height);
+    wavePath.lineTo(0, size.height);
+    wavePath.close();
+
+    final circlePath = Path();
+    circlePath.addOval(Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: size.width / 2,
+    ));
+
+    canvas.clipPath(circlePath);
+    canvas.drawPath(wavePath, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true; // For animation, you'll likely return true here.
+  }
+}
---
+++ lib/src/wave_percentage.dart
@@ -0,0 +1,147 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:percentages_with_animation/src/extensions/extensions.dart';
+
+import 'custom_painter/wave_painter.dart';
+
+class WavePercentage extends StatefulWidget {
+  /// This parameter is required and it's the current percentage value, (currentPercentage <= maxPercentage & currentPercentage >= 0.0)
+  final double currentPercentage;
+
+  /// This parameter is required and it's the maximum percentage value, (maxPercentage >= currentPercentage)
+  final double maxPercentage;
+
+  /// This parameter is the circle size for the percentage with default size 100
+  final double size;
+
+  /// This parameter is the duration for the animation with default value 1000 ms
+  final int duration;
+
+  /// This parameter is the stroke width for the background gradient circle
+  final double backgroundStrokeWidth;
+
+  /// This parameter is the circle color behind of the percentage
+  final Color backgroundColor;
+
+  /// This parameter is a custom center text with default value null (if it null then show the percentage value)
+  final String? centerText;
+
+  /// This parameter is the text style of the label for the percentage text
+  final TextStyle centerTextStyle;
+
+  /// This parameter is the call back to get the current percentage value during the animation (optional)
+  final Function(double)? onCurrentValue;
+
+  const WavePercentage({
+    super.key,
+    required this.currentPercentage,
+    required this.maxPercentage,
+    this.size = 100,
+    this.duration = 1000,
+    required this.backgroundStrokeWidth,
+    this.backgroundColor = Colors.white,
+    this.centerText,
+    this.centerTextStyle = const TextStyle(color: Colors.black),
+    this.onCurrentValue,
+  })  : assert(currentPercentage <= maxPercentage),
+        assert(currentPercentage >= 0),
+        assert(duration >= 0);
+
+  @override
+  State<WavePercentage> createState() => _WavePercentageState();
+}
+
+class _WavePercentageState extends State<WavePercentage>
+    with TickerProviderStateMixin {
+  late AnimationController _colorController;
+  late Animation<Color?> _colorAnimation;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late AnimationController _amplitudeController;
+  late Animation<double> _amplitudeAnimation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    )..repeat();
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+
+    _colorController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat();
+    _colorAnimation = ColorTween(begin: Colors.blue, end: Colors.green)
+        .animate(_colorController);
+    _amplitudeController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    )..repeat(reverse: true);
+    _amplitudeAnimation = Tween<double>(begin: 10, end: 10)
+        .animate(_amplitudeController); // Amplitude between 10 and 10
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _amplitudeController.dispose();
+    _colorController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      duration: Duration(milliseconds: widget.duration),
+      curve: Curves.easeIn,
+      tween: Tween<double>(
+        begin: 0,
+        end: widget.currentPercentage / widget.maxPercentage,
+      ),
+      builder: (context, value, _) {
+        double valueToShowOnText = value * widget.maxPercentage;
+        if (widget.onCurrentValue != null) {
+          widget.onCurrentValue!(
+              double.parse(valueToShowOnText.toStringAsFixed(2)));
+        }
+        return Container(
+          width: widget.size,
+          height: widget.size,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.transparent,
+            border: Border.all(
+              width: widget.backgroundStrokeWidth,
+              color: widget.backgroundColor,
+            ),
+          ),
+          child: AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return CustomPaint(
+                  painter: WavePainter(
+                    fillPercentage: value,
+                    waveFrequency: 2,
+                    waveAmplitude: _amplitudeAnimation.value,
+                    wavePhase: _controller.value * 2 * pi,
+                    waveColor: _colorAnimation.value!,
+                  ),
+                  child: Center(
+                    child: Text(
+                      widget.centerText ??
+                          (widget.currentPercentage.isInt == true
+                              ? valueToShowOnText.toInt().toString()
+                              : valueToShowOnText.toStringAsFixed(2)),
+                      style: widget.centerTextStyle,
+                    ),
+                  ),
+                );
+              }),
+        );
+      },
+    );
+  }
+}
