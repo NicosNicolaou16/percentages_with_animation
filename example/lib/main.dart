21b7386c8222759eb5775275a893d8fc8db220c4
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-03-31 00:52:43 +0300

    add the main class for wave perecentage

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,130 +34,163 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          const SizedBox(
-            height: 55,
-          ),
-          const Text(
-            "Linear Percentage",
-            style: TextStyle(color: Colors.black, fontSize: 21),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 59.0, left: 59),
-            child: LinearPercentage(
-              currentPercentage: 70,
-              maxPercentage: 100,
-              backgroundHeight: 20,
-              percentageHeight: 20,
-              leftRightText: LeftRightText.both,
-              showPercentageOnPercentageView: true,
-              percentageOnPercentageViewTextStyle:
-                  const TextStyle(color: Colors.white),
-              backgroundDecoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.black38,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(
+              height: 55,
+            ),
+            const Text(
+              "Linear Percentage",
+              style: TextStyle(color: Colors.black, fontSize: 21),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 59.0, left: 59),
+              child: LinearPercentage(
+                currentPercentage: 70,
+                maxPercentage: 100,
+                backgroundHeight: 20,
+                percentageHeight: 20,
+                leftRightText: LeftRightText.both,
+                showPercentageOnPercentageView: true,
+                percentageOnPercentageViewTextStyle:
+                    const TextStyle(color: Colors.white),
+                backgroundDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black38,
+                ),
+                percentageDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                ),
+                onCurrentValue: (currentValue) {
+                  if (kDebugMode) {
+                    print("LinearPercentage currentValue: $currentValue");
+                  }
+                },
               ),
-              percentageDecoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.black,
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            const Text(
+              "Circular Percentage",
+              style: TextStyle(color: Colors.black, fontSize: 21),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CircularPercentage(
+              currentPercentage: 50,
+              maxPercentage: 100,
+              size: 150,
+              duration: 2000,
+              percentageStrokeWidth: 10,
+              percentageColor: Colors.blue,
+              backgroundColor: Colors.black,
+              backgroundStrokeWidth: 2,
+              centerTextStyle: const TextStyle(
+                color: Colors.red,
+                fontSize: 21,
               ),
               onCurrentValue: (currentValue) {
                 if (kDebugMode) {
-                  print("LinearPercentage currentValue: $currentValue");
+                  print("CircularPercentage currentValue: $currentValue");
                 }
               },
             ),
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          const Text(
-            "Circular Percentage",
-            style: TextStyle(color: Colors.black, fontSize: 21),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          CircularPercentage(
-            currentPercentage: 50,
-            maxPercentage: 100,
-            size: 150,
-            duration: 2000,
-            percentageStrokeWidth: 10,
-            percentageColor: Colors.blue,
-            backgroundColor: Colors.black,
-            backgroundStrokeWidth: 2,
-            centerTextStyle: const TextStyle(
-              color: Colors.red,
-              fontSize: 21,
+            const SizedBox(
+              height: 35,
             ),
-            onCurrentValue: (currentValue) {
-              if (kDebugMode) {
-                print("CircularPercentage currentValue: $currentValue");
-              }
-            },
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          const Text(
-            "Circle Percentage",
-            style: TextStyle(color: Colors.black, fontSize: 21),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          CirclePercentage(
-            currentPercentage: 70,
-            maxPercentage: 100,
-            size: 150,
-            duration: 2000,
-            percentageColor: Colors.green,
-            backgroundStrokeWidth: 2,
-            centerTextStyle: const TextStyle(
-              color: Colors.red,
-              fontSize: 21,
+            const Text(
+              "Circle Percentage",
+              style: TextStyle(color: Colors.black, fontSize: 21),
             ),
-            onCurrentValue: (currentValue) {
-              if (kDebugMode) {
-                print("CirclePercentage currentValue: $currentValue");
-              }
-            },
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          const Text(
-            "Gradient Circle Percentage",
-            style: TextStyle(color: Colors.black, fontSize: 21),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          GradientCirclePercentage(
-            currentPercentage: 70,
-            maxPercentage: 100,
-            size: 150,
-            duration: 2000,
-            percentageStrokeWidth: 10,
-            bottomColor: Colors.green,
-            backgroundStrokeWidth: 2,
-            centerTextStyle: const TextStyle(
-              color: Colors.red,
-              fontSize: 21,
+            const SizedBox(
+              height: 15,
             ),
-            onCurrentValue: (currentValue) {
-              if (kDebugMode) {
-                print("GradientCirclePercentage currentValue: $currentValue");
-              }
-            },
-          ),
-        ],
+            CirclePercentage(
+              currentPercentage: 70,
+              maxPercentage: 100,
+              size: 150,
+              duration: 2000,
+              percentageColor: Colors.green,
+              backgroundStrokeWidth: 2,
+              centerTextStyle: const TextStyle(
+                color: Colors.red,
+                fontSize: 21,
+              ),
+              onCurrentValue: (currentValue) {
+                if (kDebugMode) {
+                  print("CirclePercentage currentValue: $currentValue");
+                }
+              },
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            const Text(
+              "Gradient Circle Percentage",
+              style: TextStyle(color: Colors.black, fontSize: 21),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GradientCirclePercentage(
+              currentPercentage: 70,
+              maxPercentage: 100,
+              size: 150,
+              duration: 2000,
+              percentageStrokeWidth: 10,
+              bottomColor: Colors.green,
+              backgroundStrokeWidth: 2,
+              centerTextStyle: const TextStyle(
+                color: Colors.red,
+                fontSize: 21,
+              ),
+              onCurrentValue: (currentValue) {
+                if (kDebugMode) {
+                  print("GradientCirclePercentage currentValue: $currentValue");
+                }
+              },
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            const Text(
+              "Wave Circle Percentage",
+              style: TextStyle(color: Colors.black, fontSize: 21),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            WavePercentage(
+              currentPercentage: 50,
+              maxPercentage: 100,
+              size: 150,
+              duration: 2000,
+              percentageStrokeWidth: 10,
+              bottomColor: Colors.green,
+              backgroundStrokeWidth: 2,
+              centerTextStyle: const TextStyle(
+                color: Colors.red,
+                fontSize: 21,
+              ),
+              onCurrentValue: (currentValue) {
+                if (kDebugMode) {
+                  print("WavePercentage currentValue: $currentValue");
+                }
+              },
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+          ],
+        ),
       ),
     );
   }
