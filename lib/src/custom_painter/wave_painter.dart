a4118520c7d08c55d3e864661b90b797186cfa73
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-03-31 21:48:13 +0300

    optimize the code for custom paint

--- lib/src/custom_painter/wave_painter.dart
+++ lib/src/custom_painter/wave_painter.dart
@@ -22,49 +22,39 @@ class WavePainter extends CustomPainter {
     final paint = Paint()
       ..color = waveColor
       ..style = PaintingStyle.fill
-      ..strokeWidth = 2.0;
+      ..strokeWidth = 1.0;
 
-    final wavePath = Path();
+    final path = Path();
     final fillHeight = size.height * (1 - fillPercentage);
     final centerX = size.width / 2;
+    double previousY;
 
-    /// Starting point with phase
-    wavePath.moveTo(
-        0,
-        fillHeight +
-            waveAmplitude *
-                sin(waveFrequency * (-centerX) / size.width * 2 * pi +
-                    pi / 2 +
-                    wavePhase));
-    double previousY = fillHeight +
+    // Use a single moveTo for the initial point
+    previousY = fillHeight +
         waveAmplitude *
             sin(waveFrequency * (-centerX) / size.width * 2 * pi +
                 pi / 2 +
                 wavePhase);
+    path.moveTo(0, previousY);
 
     for (double x = 0; x <= size.width; x++) {
-      /// Incorporate phase into the wave calculation
       final y = fillHeight +
           waveAmplitude *
               sin(waveFrequency * (x - centerX) / size.width * 2 * pi +
                   pi / 2 +
                   wavePhase);
 
-      if (x == 0) {
-        wavePath.moveTo(x, y);
-      } else {
-        wavePath.quadraticBezierTo(
-          x - 0.5,
-          (y + previousY) / 2,
-          x,
-          y,
-        );
-      }
+      path.quadraticBezierTo(
+        x - 0.5,
+        (y + previousY) / 2,
+        x,
+        y,
+      );
       previousY = y;
     }
 
-    /// Ending point with phase
-    wavePath.lineTo(
+    // Use a single lineTo for the final point
+    path.lineTo(
         size.width,
         fillHeight +
             waveAmplitude *
@@ -76,9 +66,9 @@ class WavePainter extends CustomPainter {
                     pi / 2 +
                     wavePhase));
 
-    wavePath.lineTo(size.width, size.height);
-    wavePath.lineTo(0, size.height);
-    wavePath.close();
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
 
     final circlePath = Path();
     circlePath.addOval(Rect.fromCircle(
@@ -87,11 +77,19 @@ class WavePainter extends CustomPainter {
     ));
 
     canvas.clipPath(circlePath);
-    canvas.drawPath(wavePath, paint);
+    canvas.drawPath(path, paint);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    if (oldDelegate is WavePainter) {
+      final old = oldDelegate;
+      return fillPercentage != old.fillPercentage ||
+          waveFrequency != old.waveFrequency ||
+          waveAmplitude != old.waveAmplitude ||
+          wavePhase != old.wavePhase ||
+          waveColor != old.waveColor;
+    }
     return true;
   }
 }
